8c2429f991c207ee1e9f001bfd9d321d799f4ca6
Gang Liu <68601266+golang2020@users.noreply.github.com>
2021-05-03 22:57:45 -0500

    Add files via upload
    Add 2 changed files

---
+++ DmvAppointmentScheduler/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DmvAppointmentScheduler
+{
+    class Program
+    {
+        public static Random random = new Random();
+        public static List<Appointment> appointmentList = new List<Appointment>();
+        static void Main(string[] args)
+        {
+            CustomerList customers = ReadCustomerData();
+            TellerList tellers = ReadTellerData();
+            Calculation(customers, tellers);
+            OutputTotalLengthToConsole();
+
+        }
+        private static CustomerList ReadCustomerData()
+        {
+            string fileName = "CustomerData.json";
+            string path = Path.Combine(Environment.CurrentDirectory, @"InputData\", fileName);
+            string jsonString = File.ReadAllText(path);
+            CustomerList customerData = JsonConvert.DeserializeObject<CustomerList>(jsonString);
+            return customerData;
+
+        }
+        private static TellerList ReadTellerData()
+        {
+            string fileName = "TellerData.json";
+            string path = Path.Combine(Environment.CurrentDirectory, @"InputData\", fileName);
+            string jsonString = File.ReadAllText(path);
+            TellerList tellerData = JsonConvert.DeserializeObject<TellerList>(jsonString);
+            return tellerData;
+
+        }
+        static void Calculation(CustomerList customers, TellerList tellers)
+        {
+            // Your code goes here .....
+            
+            //New Design: 
+            //1. Add new variable assignedTask in Teller.
+            //2. Sort tellers by assignedTask in order.
+            //3. Assign the customer to the first teller in the list.
+            //4. Update the assigned task value of teller after new appointment is created.
+            int numTeller = tellers.Teller.Count;
+
+            foreach(Customer customer in customers.Customer)
+            {
+                //sort the teller list 
+                tellers.Teller.Sort((x, y) => x.assignedTask.CompareTo(y.assignedTask));
+                // and assign customer to the first teller in the list
+                var appointment = new Appointment(customer, tellers.Teller[0]);
+                tellers.Teller[0].assignedTask += appointment.duration;
+                appointmentList.Add(appointment);
+
+            }
+            /*/verify result
+            List<Teller> sortedTellers = tellers.Teller.OrderBy(x => x.assignedTask).ToList();
+            Console.WriteLine("Teller 1" + sortedTellers[0].id + " will work for " + sortedTellers[0].assignedTask + " minutes!");
+            Console.WriteLine("Teller 150" + sortedTellers[numTeller-1].id + " will work for " + sortedTellers[numTeller-1].assignedTask + " minutes!");
+            */
+        }
+        static void OutputTotalLengthToConsole()
+        {
+            var tellerAppointments =
+                from appointment in appointmentList
+                group appointment by appointment.teller into tellerGroup
+                select new
+                {
+                    teller = tellerGroup.Key,
+                    totalDuration = tellerGroup.Sum(x => x.duration),
+                };
+            var max = tellerAppointments.OrderBy(i => i.totalDuration).LastOrDefault();
+            Console.WriteLine("Teller " + max.teller.id + " will work for " + max.totalDuration + " minutes!");
+        }
+
+    }
+}
---
+++ DmvAppointmentScheduler/Teller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DmvAppointmentScheduler
+{
+    /**
+     * A teller handles the request from customer.
+     */
+    public class Teller
+    {
+        public string id { get; set; }
+        public string specialtyType { get; set; }
+        public string multiplier { get; set; }
+        public double assignedTask {  get; set; }
+    }
+
+    public class TellerList
+    {
+        public List<Teller> Teller { get; set; }
+    }
+}
